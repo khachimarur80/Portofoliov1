05a89db194a2008073ca52504a55e2efae726a9b
keirh80 <khachimarur80@gmail.com>
2023-10-09 19:51:47 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-app>
+    <v-app-bar elevation="0">
+    </v-app-bar>
+    <v-main>
+      <MainFrame v-for="(point, i) in mainPoints" :key="i" :data="point" :odd="i%2!=0"/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import MainFrame from './components/MainFrame.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    MainFrame,
+  },
+
+  data: () => ({
+    mainPoints: [
+      {
+        'title' : 'Kei Rodríguez Hachimaru',
+        'text' : 'Hey, I\'m Kei.',
+        'images' : [],
+      },
+      {
+        'title' : 'About Me',
+        'text' : 'Hey, I\'m Kei.',
+        'images' : [],
+      },
+      {
+        'title' : 'My Skills',
+        'text' : 'Hey, I\'m Kei.',
+        'images' : [],
+      }
+    ]
+  }),
+}
+</script>
+
+<style>
+  body, html {
+    height: 100vh;
+    width: 100h;
+    margin: 0px;
+    padding: 0px;
+    overflow: hidden;
+  }
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  .frame:nth-child(2n) {
+    flex-direction: row-reverse !important;
+    outline: 1px solid red !important;
+  }
+</style>
---
+++ src/components/MainFrame.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <div class="frame odd" :class="odd ? 'odd frame' : 'even frame'">
+    <div class="frame-inner-left">
+      <v-card width="100%" height="100%" flat>
+        <v-card-title>
+          <div class="text-h5">
+            {{ data.title }}
+          </div>
+        </v-card-title>
+        <v-card-text>
+            {{ data.text }}
+        </v-card-text>
+        <v-card-actions>
+          <v-btn variant="outlined">
+            See more
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+    <div class="frame-inner-right">
+      <v-card  width="100%" height="100%" flat>
+        {{ data.images }}
+      </v-card>
+    </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'MainFrame',
+
+  props: {
+    data : {
+      required: true,
+    },
+    odd: {
+      required: true,
+    }
+  },
+}
+</script>
+
+<style>
+  .frame {
+    display: flex;
+    gap: 20px;
+    width: 100%;
+  }
+  .odd {
+    flex-direction: row-reverse;
+  }
+  .even {
+    flex-direction: row;
+  }
+  .frame-inner-left, .frame-inner-right {
+    width: 50% !important;
+  }
+</style>
