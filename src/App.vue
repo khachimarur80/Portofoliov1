bf0ddf979474047fd0a1e7f18a336e3219b4df4b
keirh80 <khachimarur80@gmail.com>
2023-10-10 10:23:25 +0200

    General Improvements
    - Nice Image Display
    - Basic Responsiveness
    - Frame Animation
    - Different Headers for Sections
    - Email Form (not functional yet)
    - More content

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,100 @@
 <template>
   <v-app>
     <v-app-bar elevation="0" color="teal">
+
     </v-app-bar>
-    <v-main class="overfow-auto">
-      <MainFrame v-for="(point, i) in mainPoints" :key="i" :data="point"/>
+    <v-main class="pb-10 px-2" @scroll="handleScroll" id="main">
+      <div class="text-h4 pa-3" id="kei">
+        Kei Rodríguez Hachimaru
+      </div>
+      <MainFrame v-for="(point, i) in aboutMe" :key="i" :data="point" :isOdd="i%2==0" :oneImage="point.images.length==1" ref="frames" :data-title="point.title"/>
+      <div class="text-h4 pa-3" id="projects">
+        Projects
+      </div>
+      <div class="text-h4 pa-3" id="my-github">
+        My GitHub
+      </div>
+      <div class="d-flex flex-column align-center">
+        <div class="calendar">
+        </div>
+        <v-img src="https://github-readme-stats.vercel.app/api?username=khachimarur80&show_icons=true&theme=vue-dark" width="80%" max-width="500" class="py-3"></v-img>
+        <v-img src="http://github-readme-streak-stats.herokuapp.com?user=khachimarur80&theme=vue-dark&date_format=M%20j%5B%2C%20Y%5D&card_width=468" width="80%" max-width="500" class="py-3"></v-img>
+        <v-img src="https://github-readme-stats.vercel.app/api/top-langs/?username=khachimarur80&theme=vue-dark&card_width=468" width="80%" max-width="500" class="py-3"></v-img>
+      </div>
+      <div class="text-h4 pa-3" id="contact">
+        Contact
+      </div>
+      <v-form id="contact-form" @submit.prevent="submitForm">
+        <v-card width="100%" height="100%" flat class="d-flex flex-column pa-3">
+          <input type="hidden" name="contact_number" v-model="formData.contact_number">
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                solo
+                v-model="formData.from_name"
+                label="Name"
+                required
+                :rules="nameRules"
+                variant="outlined"
+                density="compact"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="formData.user_email"
+                label="Email"
+                required
+                type="email"
+                :rules="emailRules"
+                variant="outlined"
+                density="compact"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-textarea
+                regular
+                v-model="formData.message"
+                label="Message"
+                required
+                :rules="messageRules"
+                no-resize
+                auto-grow
+                variant="outlined"
+                density="compact"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="10">
+            </v-col>
+            <v-col cols="2">
+              <v-btn type="submit" color="teal-lighten-2" variant="outlined">Send</v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-form>
+      <v-btn
+        v-if="showButton"
+        class="scroll-to-top"
+        color="teal-lighten-2"
+        fab
+        icon
+        outlined
+        @click="scrollToTop"
+      >
+        <v-icon>mdi-chevron-up</v-icon>
+      </v-btn>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import MainFrame from './components/MainFrame.vue'
+
 
 export default {
   name: 'App',
@@ -19,27 +104,92 @@ export default {
   },
 
   data: () => ({
-    mainPoints: [
+    aboutMe: [
       {
-        'title' : 'Kei Rodríguez Hachimaru',
-        'text' : 'Hey, I\'m Kei.',
-        'images' : [
-          require('@/assets/images/Logo.png')
-        ],
-      },
-      {
+        'show' : false,
         'title' : 'About Me',
         'text' : 'I\'m a first year CS student who lives in Madrid. I enjoy playing the violin, various sports, and of course, coding. I love building and learning stuff!',
         'images' : [
+          require('@/assets/images/Retiro.jpg')
         ],
       },
       {
+        'show' : false,
         'title' : 'My Skills',
         'text' : 'Hey, I\'m Kei.',
-        'images' : [],
+        'images' : [
+          require('@/assets/images/html5-original.svg'),
+          require('@/assets/images/css3-original.svg'),
+          require('@/assets/images/python-original.svg'),
+          require('@/assets/images/django-plain.svg'),
+          require('@/assets/images/javascript-plain.svg'),
+          require('@/assets/images/vuejs-original.svg'),
+          require('@/assets/images/vuetify-original.svg'),
+          require('@/assets/images/nodejs-original.svg'),
+          require('@/assets/images/electron-original.svg'),
+        ],
       }
-    ]
+    ],
+    formData: {
+      contact_number: '',
+      from_name: '',
+      user_email: '',
+      message: '',
+    },
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 50) || 'Name must be less than 50 characters',
+    ],
+    emailRules: [
+      v => !!v || 'Email is required',
+      v => /.+@.+\..+/.test(v) || 'Email must be valid',
+    ],
+    messageRules: [
+      v => !!v || 'Message is required',
+      v => (v && v.length <= 500) || 'Message must be less than 500 characters',
+    ],
+    showButton: false,
   }),
+
+  methods: {
+    submitForm() {
+      // You can handle form submission logic here
+      // Access form data via this.formData
+      console.log(this.formData);
+      // You can use Vue resource or Axios to send data to the server
+      // Example using Axios:
+      // axios.post('/your-api-endpoint', this.formData)
+      //   .then(response => {
+      //     // Handle success
+      //   })
+      //   .catch(error => {
+      //     // Handle error
+      //   });
+    },
+    handleScroll() {
+      const mainElement = document.getElementById('main');
+      this.showButton = mainElement.scrollTop > 100;
+
+      const frames = this.$refs.frames;
+
+      if (frames) {
+        frames.forEach((frame, index) => {
+          if (frame) {
+            const rect = frame.$el.getBoundingClientRect();
+            const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+            const isFrameInViewport = rect.top >= -rect.height/4 && rect.bottom <= windowHeight + rect.height/4
+
+            this.aboutMe[index].show = isFrameInViewport
+          }
+        });
+      }
+    },
+    scrollToTop() {
+      const mainElement = document.getElementById('main');
+      mainElement.scrollTo({ top: 0, behavior: 'smooth' });
+    },
+  },
 }
 </script>
 
@@ -54,7 +204,37 @@ export default {
   ::-webkit-scrollbar {
     display: none;
   }
-  .frame:nth-child(2n) {
-    flex-direction: row-reverse !important;
+  #main {
+    height: 100vh;
+    width: 100%;
+    overflow: scroll;
+  }
+  .scroll-to-top {
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    transition: opacity 0.3s ease-in-out;
+    opacity: 0.7;
+    z-index: 1000;
+  }
+  @media screen and (max-width: 600px) {
+    #contact-form {
+      width: 80%;
+      margin-left: 10%;
+    }
+  }
+
+  @media screen and (min-width: 601px) and (max-width: 1024px) {
+    #contact-form {
+      width: 80%;
+      margin-left: 10%;
+    }
+  }
+
+  @media screen and (min-width: 1025px) {
+    #contact-form {
+      width: 80%;
+      margin-left: 10%;
+    }
   }
 </style>
--- src/components/MainFrame.vue
+++ src/components/MainFrame.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="even frame pt-3 pl-3 pr-3">
+  <div 
+    :class="[
+      'frame', 
+      isOdd ? 'odd' : 'even', 
+      'pa-3', 
+      oneImage  ? 'one-image' : '' ,
+      data.show  ? 'show-frame' : '' 
+    ]" 
+    ref="frame">
     <div class="frame-inner-left">
-      <v-card width="100%" height="100%" flat class="d-flex flex-column">
+      <v-card width="100%" height="100%" flat class="d-flex flex-column" color="transparent">
         <v-card-title>
           <div class="text-h5">
             {{ data.title }}
@@ -20,10 +28,16 @@
       </v-card>
     </div>
     <div class="frame-inner-right">
-      <v-card  width="100%" height="100%" flat>
-        <v-img v-for="(image, i) in data.images" :key="i" :src="image">
-        </v-img>
-      </v-card>
+      <v-row v-if="data.images.length == 9">
+        <v-col
+          v-for="(image, i) in data.images.slice(0, 9)"
+          :key="i"
+          cols="4"
+        >
+          <v-img :src="image" contain></v-img>
+        </v-col>
+      </v-row>
+      <v-img :src="data.images[0]" contain width="100%" height="100%" v-else></v-img>
     </div>
   </div>
 </template>
@@ -36,6 +50,15 @@ export default {
   props: {
     data : {
       required: true,
+    },
+    isOdd: {
+      required: true,
+    },
+    oneImage: {
+      required: true,
+    },
+    showFrame: {
+      required: true
     }
   },
 }
@@ -44,17 +67,60 @@ export default {
 <style>
   .frame {
     display: flex;
+    justify-content: center;
+    align-items: center;
     gap: 12px;
     width: 100%;
+    opacity: 0;
+    transition: opacity 0.5s, transform 0.5s;
   }
   .odd {
     flex-direction: row-reverse;
+    transform: translateX(-100%);
   }
   .even {
     flex-direction: row;
+    transform: translateX(100%);
+  }
+  .show-frame {
+    transform: translateX(0) !important;
+    opacity: 1 !important;
   }
   .frame-inner-left, .frame-inner-right {
-    width: 50% !important;
-    min-height: 200px;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media screen and (max-width: 600px) {
+    .frame {
+      max-height: 600px;
+    }
+    .one-image.odd {
+      flex-direction: column-reverse;
+    }
+    .one-image.even {
+      flex-direction: column;
+    }
+
+
+    .frame-inner-left, .frame-inner-right {
+      width: 100%;
+      height: 210px;
+    }
+  }
+
+  @media screen and (min-width: 601px) and (max-width: 1024px) {
+    .frame {
+      max-height: 250px;
+    }
+  }
+
+  @media screen and (min-width: 1025px) {
+    .frame {
+      max-height: 300px;
+    }
   }
 </style>
